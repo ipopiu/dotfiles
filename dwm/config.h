d4feb76ddc18af6cc50600319a98a71f07b10a92
ipopiu <piufreek@gmail.com>
2024-08-04 15:07:15 +0530

    removed colors

--- dwm/config.h
+++ dwm/config.h
@@ -10,12 +10,12 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Code:size=11" };
 static const char dmenufont[]       = "Fira Code:size=11";
 
-static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#644475";
-static const char col_gray3[]       = "#f8f8f2";
-static const char col_gray4[]       = "#282a36";
-static const char col_gray5[]       = "#a272a4";
-static const char col_cyan[]        = "#BD93F9";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+
 
 
 static const char *colors[][3]      = {
